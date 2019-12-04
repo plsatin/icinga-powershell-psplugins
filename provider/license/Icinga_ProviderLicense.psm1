e0978e27b54b1fb8580606ba914b3cb040dd5207
Павел Сатин <pslater.ru@gmail.com>
2019-12-04 18:47:04 +0500

    comment test enums

--- provider/license/Icinga_ProviderLicense.psm1
+++ provider/license/Icinga_ProviderLicense.psm1
@@ -7,18 +7,18 @@ function Get-IcingaSoftwareLicensingProduct {
 
     <# #>
 
-    [hashtable]$LicenseStatusDescription = @{
-        0 = 'Unlicensed';
-        1 = 'Licensed';
-        2 = 'Out-Of-Box Grace Period';
-        3 = 'Out-Of-Tolerance Grace Period';
-        4 = 'Non-Genuine Grace Period';
-        5 = 'Notification';
-        6 = 'Extended Grace';
-    }
+    # [hashtable]$LicenseStatusDescription = @{
+    #     0 = 'Unlicensed';
+    #     1 = 'Licensed';
+    #     2 = 'Out-Of-Box Grace Period';
+    #     3 = 'Out-Of-Tolerance Grace Period';
+    #     4 = 'Non-Genuine Grace Period';
+    #     5 = 'Notification';
+    #     6 = 'Extended Grace';
+    # }
 
-    $ErrorActionPreference = "SilentlyContinue"
-    $PSProviderEnums.Add('LicenseStatusDescription', $LicenseStatusDescription);
+    # $ErrorActionPreference = "SilentlyContinue"
+    # $PSProviderEnums.Add('LicenseStatusDescription', $LicenseStatusDescription);
 
 
 
